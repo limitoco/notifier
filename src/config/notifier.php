730548fb838acf25763dec9ed0d5bac8b89eae1f
Mohsen Shahbazi <msahost@gmail.com>
2019-07-30 23:19:02 +0430

    Update notifier.php

--- src/config/notifier.php
+++ src/config/notifier.php
@@ -14,23 +14,23 @@ return [
         'gateways' => [
             'payamresan' => [
                 'url' => 'http://sms-webservice.ir/v1/v1.asmx?WSDL',
-                'username' => '09122770270',
-                'password' => 'm@neyz85',
-                'from' => '500020607608',
+                'username' => '0',
+                'password' => '0',
+                'from' => '0',
                 'class_name' => Limito\Notifier\Providers\Sms\Gateways\PayamResan::class
             ],
             'kavehnegar' => [
-                'url' => 'https://api.kavenegar.com/v1/6A5444597961587670525933613152796A6E4E5455673D3D/sms/send.json?',
-                'username' => 'info@noonreson.ir',
-                'password' => '6A5444597961587670525933613152796A6E4E5455673D3D',
-                'from' => '10000101011110',
+                'url' => 'https://api.kavenegar.com/v1/{token}/sms/send.json?',
+                'username' => '0',
+                'password' => '0',
+                'from' => '0',
                 'class_name' => Limito\Notifier\Providers\Sms\Gateways\KavehNegar::class
             ],
             'kavehnegar_verify' => [
-                'url' => 'https://api.kavenegar.com/v1/6A5444597961587670525933613152796A6E4E5455673D3D/sms/send.json?',
-                'username' => 'info@noonreson.ir',
-                'password' => '6A5444597961587670525933613152796A6E4E5455673D3D',
-                'from' => '10000101011110',
+                'url' => 'https://api.kavenegar.com/v1/{token}/sms/send.json?',
+                'username' => '0',
+                'password' => '0',
+                'from' => '0',
                 'class_name' => Limito\Notifier\Providers\Sms\Gateways\KavehNegarVerify::class
             ]
         ]
